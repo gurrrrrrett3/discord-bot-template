020eb690107bf757825483da5d3cf08b6dd1c3d9
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-08-17 16:36:35 -0400

    add interaction managers

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,11 +1,18 @@
 import { Client } from "discord.js";
-import CommandLoader from "./loaders/commandLoader";
 import ModuleLoader from "./loaders/moduleLoader";
+import CommandLoader from "./loaders/commandLoader";
+import ButtonManager from "./loaders/managers/buttonManager";
+import SelectMenuManager from "./loaders/managers/selectMenuManager";
+import ModalManager from "./loaders/managers/modalManager";
 
 export default class Bot {
 
     public moduleLoader: ModuleLoader
     public commandLoader: CommandLoader
+
+    public buttonManager: ButtonManager
+    public selectMenuManager: SelectMenuManager
+    public modalManager: ModalManager
   
   constructor(public client: Client) {
     this.client
@@ -16,5 +23,8 @@ export default class Bot {
     this.moduleLoader = new ModuleLoader(this);
     this.commandLoader = new CommandLoader(this.client);
     
+    this.buttonManager = new ButtonManager(this.client);
+    this.selectMenuManager = new SelectMenuManager(this.client);
+    this.modalManager = new ModalManager(this.client);
   }
 }
