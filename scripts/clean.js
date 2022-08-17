0820fc71c014493a80d7bff4340fd3114be2901f
gurrrrrrett3 <61361654+gurrrrrrett3@users.noreply.github.com>
2022-08-17 16:56:11 -0400

    fix clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,14 @@
 const fs = require("fs")
 
-fs.unlinkSync("./bot/modules/example/")
-fs.unlinkSync("./bot/modules/README.md")
-fs.unlinkSync("./bot/loaders/README.md")
+fs.rmSync("./bot/modules/example/", {
+    recursive: true,
+    force: true
+})
+fs.rmSync("./scripts", {
+    recursive: true,
+    force: true
+})
+fs.rmSync("./bot/modules/README.md")
+fs.rmSync("./bot/loaders/README.md")
 
 console.log("Cleaned up!")
